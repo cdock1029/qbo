cf265e613baa0e73ff10174f11188cdff2cccb40
Conor Dockry <conordockry@gmail.com>
2015-02-15 16:20:45 +0000

    simple login screen, and redirect to close after intuit flow

--- server.js
+++ server.js
@@ -1,47 +1,154 @@
 var Hapi = require('hapi'),
     Path = require('path'),
     Request = require('request'),
-    qs = require('querystring'),
+    Qs = require('qs'),
+    
+    AWS = require('aws-sdk'),
+    DOC = require("dynamodb-doc"),
 
     QuickBooks = require('node-quickbooks');
 
-var server = new Hapi.Server({
+var testUser = process.env.QBO_USER; 
+var users = {};
+
+users[testUser] = {
+    id: testUser,
+    password: process.env.QBO_PW,
+    name: 'Bill Brasky'
+};
+
+AWS.config.update({region: 'us-east-1'});
+var docClient = new DOC.DynamoDB();
+
+var server = new Hapi.Server();//{
+    /*
     connections: {
         routes: {
             files: {
                 relativeTo: Path.join(__dirname, '.')
             }
         }
-    }
-});
+    }*//*,
+    debug: { request: ['log', 'response', 'error', 'request'] }
+    */
+//});
+
 
 var consumerKey = process.env.CONSUMER_KEY,
     consumerSecret = process.env.CONSUMER_SECRET;
     
-var ip = process.env.IP,
-    port = process.env.PORT;
+var {IP, PORT, C9_HOSTNAME}  = process.env;
 
 server.connection({
-    address: ip,
-    port: port
+    host: C9_HOSTNAME,
+    address: IP,
+    port: PORT
 });
 
-server.register({
-    register: require('yar'),
-    options: {
-        cookieOptions: {
-            password: 'password',
-            isSecure: false
+var home = (request, reply) => {
+
+    reply('<html><head><title>Login page</title></head><body><h3>Welcome '
+      + request.auth.credentials.name
+      + '!</h3><br/><form method="get" action="/logout">'
+      + '<input type="submit" value="Logout">'
+      + '</form></body></html>');
+};
+
+var login = (request, reply) => {
+
+    if (request.auth.isAuthenticated) {
+        return reply.redirect('/');
+    }
+
+    var message = '';
+    var account = null;
+
+    if (request.method === 'post') {
+
+        if (!request.payload.username ||
+            !request.payload.password) {
+
+            message = 'Missing username or password';
+        }
+        else {
+            account = users[request.payload.username];
+            if (!account ||
+                account.password !== request.payload.password) {
+
+                message = 'Invalid username or password';
+            }
         }
     }
-}, (err) => {
+
+    if (request.method === 'get' ||
+        message) {
+
+        return reply('<html><head><title>Login page</title></head><body>'
+            + (message ? '<h3>' + message + '</h3><br/>' : '')
+            + '<form method="post" action="/login">'
+            + 'Username: <input type="text" name="username"><br>'
+            + 'Password: <input type="password" name="password"><br/>'
+            + '<input type="submit" value="Login"></form></body></html>');
+    }
+
+    request.auth.session.set(account);
+    return reply.redirect('/');
+};
+
+var logout = function (request, reply) {
+
+    request.auth.session.clear();
+    return reply.redirect('/');
+};
+
+
+server.register(require('hapi-auth-cookie'), function (err) {
     if (err) {
-        console.log('Failed to load yar plugin.');
-        throw err;
+        console.err(err);
+    } else {
+        server.auth.strategy('session', 'cookie', {
+            password: 'secret',
+            cookie: 'sid-example',
+            redirectTo: '/login',
+            isSecure: true
+        });
     }
 });
 
-server.route({
+server.route([
+   {
+        method: 'GET',
+        path: '/',
+        config: {
+            handler: home,
+            auth: 'session'
+        }
+    },
+    {
+        method: ['GET', 'POST'],
+        path: '/login',
+        config: {
+            handler: login,
+            auth: {
+                mode: 'try',
+                strategy: 'session'
+            },
+            plugins: {
+                'hapi-auth-cookie': {
+                    redirectTo: false
+                }
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/logout',
+        config: {
+            handler: logout,
+            auth: 'session'
+        }
+    }, 
+    /*{
     method: 'GET',
     path: '/src/{param*}',
     handler: {
@@ -49,9 +156,7 @@ server.route({
             path: 'src'
         }
     }
-});
-
-server.route({
+},{
     method: 'GET',
     path: '/{param*}',
     handler: {
@@ -60,72 +165,73 @@ server.route({
             index: true
         }
     }
-});
-
-server.route({
+},*/{
     method: 'GET',
     path: '/oauth/start',
+    config: {
     handler: (request, reply) => {
         var ctx = {
-            port: port,
+            user: request.session.get('user'),
+            grantUrl: 'https://' + C9_HOSTNAME + '/oauth/requestToken',
             appCenter: QuickBooks.APP_CENTER_BASE
         };
-        reply.view('intuit', ctx);
+        reply.view('intuit.ejs', ctx);
+    },
+    auth: 'session'
     }
-});
-
-server.route({
+},{
     method: 'GET',
     path: '/oauth/requestToken',
+    config: {
     handler: (request, reply) => {
         var postBody = {
             url: QuickBooks.REQUEST_TOKEN_URL,
             oauth: {
-                callback: 'http://' + ip + ':' + port + '/oauth/callback',
+                callback: 'https://' + C9_HOSTNAME + '/oauth/callback',
                 consumer_key: consumerKey,
                 consumer_secret: consumerSecret
             }
         };
         Request.post(postBody, (e, r, data) => {
-            var requestToken = qs.parse(data);
-
-            var qboSession = request.session.get('qbo_session');
-            console.log('session: ' + qboSession);
-            //if (!qboSession) {
-                qboSession = { oauth_token_secret: requestToken.oauth_token_secret };
-            //}
-            qboSession.last = Date.now();
-            console.log(requestToken);
-            request.session.set('qbo_session', qboSession);
+            var requestToken = Qs.parse(data);
+            console.info('after post to QBO Reuqest token url.\nRequest Token: ');
+            console.info(requestToken);
+            request.session.set('oauth_token_secret', requestToken.oauth_token_secret);
             reply.redirect(QuickBooks.APP_CENTER_URL + requestToken.oauth_token);
         });
-    }
-});
-
-server.route({
+    },
+    auth: 'session'}
+},{
     method: 'GET',
     path: '/oauth/callback',
+    config: {
     handler: (request, reply) => {
-        console.log('oauth callback');
-        var qboSession = request.session.get('qbo_session');
-        console.log(qboSession);
+        console.info('In oauth callback');
         var postBody = {
             url: QuickBooks.ACCESS_TOKEN_URL,
             oauth: {
                 consumer_key: consumerKey,
                 consumer_secret: consumerSecret,
                 token: request.query.oauth_token,
-                token_secret: qboSession.oauth_token_secret,
+                token_secret: request.session.get('oauth_token_secret'),
                 verifier: request.query.oauth_verifier,
                 realmId: request.query.realmId
             }
         };
         Request.post(postBody, (e, r, data) => {
-            var accessToken = qs.parse(data);
-            console.log(accessToken);
-            console.log(postBody.oauth.realmId);
-
-            qbo = new QuickBooks(
+            var accessToken = Qs.parse(data);
+            console.info('after post to access token url.\n Access Token: ');
+            console.info(accessToken);
+           
+            request.session.set('user', {
+                oauth_token: accessToken.oauth_token,
+                oauth_token_secret: accessToken.oauth_token_secret,
+                realmId: postBody.oauth.realmId
+            });
+            console.info('redirect to close here');
+            reply.redirect('https://' + C9_HOSTNAME + '/close');
+            
+            /*qbo = new QuickBooks(
                 consumerKey,
                 consumerSecret,
                 accessToken.oauth_token,
@@ -136,21 +242,76 @@ server.route({
             );
             qbo.getCompanyInfo('1327360235', (_, companyInfo) => {
                 reply(companyInfo);
-            });
+            });*/
         });
-    }
-});
+    },
+    auth: 'session'}
+},{
+    method: 'GET',
+    path: '/close',
+    config: {
+    handler: (request, reply) => {
+        reply.view('close.html');      
+    },
+    auth: 'session'}
+    
+}]);
 
 server.views({
     engines: {
-        ejs: {
-            module: require('ejs')
-        },
+        ejs: require('ejs'),
         html: require('handlebars')
     },
     path: 'src/views'
 });
 
-server.start(() => {
-    console.log('server started at ' + server.info.uri);
+server.register({
+    register: require('yar'),
+    options: {
+        cookieOptions: {
+            password: 'password',
+            isSecure: true
+        }
+    }
+}, (err) => {
+    if (err) {
+        console.error('Failed to load yar plugin.');
+        throw err;
+    }
 });
+
+
+
+var options = {
+    opsInterval: 1000,
+    reporters: [{
+        reporter: require('good-console'),
+        args:[{ log: '*', response: '*' , error: '*', request: '*'}]
+    }/*, {
+        reporter: require('good-file'),
+        args: ['./test/fixtures/awesome_log', { ops: '*' }]
+    }, {
+        reporter: require('good-http'),
+        args: [{ error: '*' }, 'http://prod.logs:3000', {
+            threshold: 20,
+            wreck: {
+                headers: { 'x-api-key' : 12345 }
+            }
+        }]
+    }*/]
+};
+
+server.register({
+    register: require('good'),
+    options: options
+}, (err) => {
+
+    if (err) {
+        console.error(err);
+    } else {
+        server.start(() => {
+            console.info('server started @ ' + IP + ':' + PORT + '\naccess @ ' + C9_HOSTNAME);
+        });
+    }
+});
+
